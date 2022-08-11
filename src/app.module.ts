32e4cb09ffe2baf59fcf9b0bb305b947ad311e6b
Sextus Empiricus <8272331@gmail.com>
2022-08-11 10:54:37 +0200

    finished

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { CatModule } from './cat/cat.module';
-import { SetDummyModule } from './set-dummy/set-dummy.module';
-import { config } from '../config/config';
 import { CustomCacheModule } from './custom-cache/custom-cache.module';
+import { SetDummyModule } from './set-dummy/set-dummy.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { config } from '../config/config';
+import { AuthModule } from './auth/auth.module';
 
 const { host, port, database, password, username } = config.db;
 
@@ -19,13 +21,15 @@ const { host, port, database, password, username } = config.db;
          password,
          database,
          entities: ['./dist/**/*.entity{.js,.ts}'],
-         // logging: true,
+         logging: true,
          bigNumberStrings: false,
          synchronize: true,
       }),
       CatModule,
-      SetDummyModule,
       CustomCacheModule,
+      SetDummyModule,
+      UserModule,
+      AuthModule,
    ],
    controllers: [AppController],
    providers: [AppService],
